40cf9f7aa358fc6511a916c6f5079c43b285a5f3
Miami78 <miamilarry78@gmail.com>
2020-03-15 18:37:29 +0300

    CI fix

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ const config = require("./Utils/config");
 const PORT = config.SERVER_PORT;
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
 
 export default app;
