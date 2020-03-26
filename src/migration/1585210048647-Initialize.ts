181791c1370d9d9326bc038d8703f9eb622d6666
Miami78 <miamilarry78@gmail.com>
2020-03-26 11:24:06 +0300

    add user migrations

--- src/migration/1585210048647-Initialize.ts
+++ src/migration/1585210048647-Initialize.ts
@@ -4,7 +4,6 @@ export class Initialize1585210048647 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
         await queryRunner.query(`CREATE TABLE "user" ("id" SERIAL NOT NULL, "firstName" character varying NOT NULL, "lastName" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`, undefined);
-        await queryRunner.query(`ALTER TABLE "user"`, undefined);
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
